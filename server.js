9ca93ca6c46624014e2166b3f1b020aec8ecc7d8
notigayatri <gayatrin005@gmail.com>
2025-04-04 01:14:35 +0530

    changed files

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 // Import required modules
 const express = require('express');
 const cors = require('cors');
-const bodyParser = require('body-parser');
 require('dotenv').config();
 const connectDB = require('./db');
 const path = require('path');
-const open = require('open')
+const open = await import('open');
+
+
 
 // Initialize Express app
 const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());  // Enable CORS
-app.use(bodyParser.json()); // Parse JSON request body
+app.use(cors({ origin: 'http://localhost:3000' }));  // Enable CORS
 app.use(express.json());  // Alternative to body-parser
 
 // Connect to MongoDB
@@ -25,7 +25,8 @@ const { generateGeminiResponse } = require('./services/geminiService');
 
 app.use('/api', Routes);
 // Serve static files from the "public" directory
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname, 'public', 'index.html')));
+
 // Default route
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
@@ -36,5 +37,5 @@ const { exec } = require('child_process');
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
-    exec(`start http://localhost:${port}`); // Windows
+    open(`http://localhost:${port}`);  // Windows
 });
