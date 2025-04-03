95952ef890d6ed280061ebf43ec01b4c7f3ae925
notigayatri <gayatrin005@gmail.com>
2025-04-04 01:23:34 +0530

    changed server

--- server.js
+++ server.js
@@ -4,9 +4,7 @@ const cors = require('cors');
 require('dotenv').config();
 const connectDB = require('./db');
 const path = require('path');
-const open = await import('open');
-
-
+const open = require('open'); // Changed from await import('open')
 
 // Initialize Express app
 const app = express();
@@ -24,12 +22,13 @@ const Routes = require('./routes/modelRoutes');
 const { generateGeminiResponse } = require('./services/geminiService');
 
 app.use('/api', Routes);
+
 // Serve static files from the "public" directory
-app.use(express.static(path.join(__dirname, 'public', 'index.html')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Default route
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // Start the server
@@ -37,5 +36,7 @@ const { exec } = require('child_process');
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
-    open(`http://localhost:${port}`);  // Windows
+
+    // Open the URL in the default browser
+    open(`http://localhost:${port}`).catch(err => console.error("Error opening browser:", err));
 });
